bab2f4eee885d4a9c6aa0316fcba2a5bc873e343
mbilalprotopie <bilal@protopie.io>
2023-05-28 19:30:00 +0900

    i

--- buy_lotto copy.py
+++ buy_lotto copy.py
@@ -63,53 +63,53 @@ def run():
     element.send_keys(Keys.ENTER)
     WebDriverWait(driver, 40).until(EC.staleness_of(element))
 
-    # driver.get("https://ol.dhlottery.co.kr/olotto/game/game645.do")
+    driver.get("https://ol.dhlottery.co.kr/olotto/game/game645.do")
 
-    # parent = driver.current_window_handle
-    # uselessWindows = driver.window_handles
-    # for winId in uselessWindows:
-    #     if winId != parent: 
-    #         driver.switch_to.window(winId)
-    #         driver.close()
-    #         driver.switch_to.window(parent)
+    parent = driver.current_window_handle
+    uselessWindows = driver.window_handles
+    for winId in uselessWindows:
+        if winId != parent: 
+            driver.switch_to.window(winId)
+            driver.close()
+            driver.switch_to.window(parent)
 
-    # # # Wait for the popup to disappear
-    # # WebDriverWait(driver, 20).until(EC.invisibility_of_element((By.ID, "popupLayerAlert")))
+    # # Wait for the popup to disappear
+    # WebDriverWait(driver, 20).until(EC.invisibility_of_element((By.ID, "popupLayerAlert")))
 
 
-    # # Click text=자동번호발급
-    # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[text()="자동번호발급"]')))
-    # # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'num2')))
+    # Click text=자동번호발급
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[text()="자동번호발급"]')))
+    # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, 'num2')))
+    element.click()
+    time.sleep(5)
+
+    # # Select 1 for 구매할 개수를 선택
+    select = Select(driver.find_element(By.ID, 'amoundApply'))  # replace 'select' with the actual name or id of the select tag
+    select.select_by_value(str(COUNT))
+    time.sleep(5)
+
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnSelectNum"))).click()
+
+    # # Click input:has-text("구매하기")
+    # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[contains(text(),"구매하기")]')))
     # element.click()
-    # time.sleep(5)
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnBuy"))).click()
+    time.sleep(2)
 
-    # # # Select 1 for 구매할 개수를 선택
-    # select = Select(driver.find_element(By.ID, 'amoundApply'))  # replace 'select' with the actual name or id of the select tag
-    # select.select_by_value(str(COUNT))
-    # time.sleep(5)
-
-    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnSelectNum"))).click()
-
-    # # # Click input:has-text("구매하기")
-    # # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//input[contains(text(),"구매하기")]')))
-    # # element.click()
-    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.ID, "btnBuy"))).click()
-    # time.sleep(2)
-
-    # #clicking 확인 button
-    # WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popupLayerConfirm"]/div/div[2]/input[1]'))).click()
+    #clicking 확인 button
+    WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.XPATH, '//*[@id="popupLayerConfirm"]/div/div[2]/input[1]'))).click()
 
 
-    # # # Click input[name="closeLayer"]
-    # element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'closeLayer')))
-    # element.click()
+    # # Click input[name="closeLayer"]
+    element = WebDriverWait(driver, 10).until(EC.element_to_be_clickable((By.NAME, 'closeLayer')))
+    element.click()
 
-    # # # assert page.url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
-    # # assert driver.current_url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
-    # time.sleep(10)
+    # # assert page.url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
+    # assert driver.current_url == "https://el.dhlottery.co.kr/game/TotalGame.jsp?LottoId=LO40"
+    time.sleep(10)
 
-    # driver.close()
-    # driver.quit()
+    driver.close()
+    driver.quit()
 
 run()
 
